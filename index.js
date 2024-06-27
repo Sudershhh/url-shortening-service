47fcb150aff00e206c41cfbc40b2d4394ac3b0fc
Sri Sudersan Thopey Ganesh <tgsuder@gmail.com>
2024-06-27 04:46:54 +0000

    Added Invalid URL check

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyparser = require('body-parser')
+const url = require('url');
+const isUrl = require('is-url')
+
+let counter = 1;
+
+let shortenedUrls={}
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +16,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +29,35 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+app.get('/api/shorturl/:short_url',function(req,res){
+
+  let urlCount = req.params.short_url;
+  res.redirect(shortenedUrls[urlCount]);
+
+})
+
+app.post('/api/shorturl', function(req,res){
+
+  const url = req.body.url
+
+
+  //Invalid URL test
+  if(!isUrl(url))
+  {
+    res.send({ error: 'invalid url' })
+    return
+  }
+  
+
+  shortenedUrls[counter] = url;
+  
+  res.send({ original_url : url, short_url : counter})
+  counter++;
+
+
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
